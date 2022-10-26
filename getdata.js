71ecc62385e044f9be6c2d1529772b6442da1a0b
wanofuhito <116492669+wanofuhito@users.noreply.github.com>
2022-10-26 21:08:05 +0900

    Create getdata.js

---
+++ getdata.js
@@ -0,0 +1,37 @@
+<script>
+  function get_download_url(){
+    const url = window.location.href;
+    const filename = url.match(".+/(.+?)\.[a-z]+([\?#;].*)?$")[1] + '.txt';
+    const pathname = location.pathname.split("/");
+    const foldername = pathname[pathname.length-2];
+    const download_url = window.location.protocol + '//' + window.location.hostname + '/'+ foldername + '/' + filename;
+    console.log(download_url);
+    return download_url;
+  }
+  function get_data() {
+    return new Promise((resolve, reject) => {
+        //axios.get('https://wanofuhito.github.io/nihon-shoki/14rolls.txt')
+        axios.get(get_download_url())
+            .then(function (response) {
+                // 成功時に実行
+                // response.dataに実際のデータが入っている
+                let result = response.data;
+                resolve(result);
+            })
+            .catch(function (error) {
+                // エラー時に実行
+                reject(error);
+            })
+            .then(function () {
+                // 常に実行
+            });
+    });
+  }
+  async function convert(id) {
+    let data = await get_data();
+    console.log(data);
+    document.getElementById("container").innerHTML =  "<p>" + data.replace(/\n/g, "</p><p>") + "</p>";
+    convertKanbunDiv(document.getElementById(id));
+  }
+     
+</script>
