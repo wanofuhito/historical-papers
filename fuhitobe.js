eafffbc6fdcb9924755d70db2d5e9d25a624acea
wanofuhito <116492669+wanofuhito@users.noreply.github.com>
2022-10-29 14:26:11 +0900

    Update fuhitobe.js

--- fuhitobe.js
+++ fuhitobe.js
@@ -1,45 +1,59 @@
-  function get_download_url(file){
+  let raw_data;
+  let tagged_data;
+  function get_download_url(file) {
     console.log(file)
     const url = window.location.href;
     let filename = url.match(".+/(.+?)\.[a-z]+([\?#;].*)?$")[1] + '.txt';
-    if (file != null)
-      filename = file;
+    if (file != null) filename = file;
     const pathname = location.pathname.split("/");
-    const foldername = pathname[pathname.length-2];
-    const download_url = window.location.protocol + '//' + window.location.hostname + '/'+ foldername + '/' + filename;
+    const foldername = pathname[pathname.length - 2];
+    const download_url = window.location.protocol + '//' + window.location.hostname + '/' + foldername + '/' + filename;
     console.log(download_url);
     return download_url;
   }
+
   function get_data(file) {
     return new Promise((resolve, reject) => {
-        //axios.get('https://wanofuhito.github.io/nihon-shoki/14rolls.txt')
-        axios.get(get_download_url(file))
-            .then(function (response) {
-                // 成功時に実行
-                // response.dataに実際のデータが入っている
-                let result = response.data;
-                resolve(result);
-            })
-            .catch(function (error) {
-                // エラー時に実行
-                reject(error);
-            })
-            .then(function () {
-                // 常に実行
-            });
+      axios.get(get_download_url(file)).then(function (response) {
+        // 成功時に実行
+        // response.dataに実際のデータが入っている
+        let result = response.data;
+        resolve(result);
+      }).catch(function (error) {
+        // エラー時に実行
+        reject(error);
+      }).then(function () {
+        // 常に実行
+      });
     });
   }
-  async function convert(id,file) {
+  async function convert(id, file) {
     console.log(file);
     let data = await get_data(file);
     console.log(data);
-    document.getElementById(id).innerHTML =  "<p>" + data.replace(/\n/g, "</p><p>") + "</p>";
+    raw_data = data;
+    document.getElementById(id).innerHTML = "<p>" + data.replace(/\n/g, "</p><p>") + "</p>";
     convertKanbunDiv(document.getElementById(id));
+    tagged_data = document.getElementById(id).innerHTML;
   }
+
   function toHTMLNone(str) {
-      return str;
+    return str;
   }
+
   function convertKanaDiv(id) {
-      div = document.getElementById(id)
-      div.childNodes.forEach(function (p) { p.innerHTML = toHTMLNone(p.textContent); });
+    div = document.getElementById(id)
+    div.childNodes.forEach(function (p) {
+      p.innerHTML = toHTMLNone(p.textContent);
+    });
+  }
+
+  function copy_to_clipboard(copy_data) {
+    navigator.clipboard.writeText(copy_data).then(
+      () => {
+        /* clipboard successfully set */
+        alert('コピーしました')
+      }, () => {
+        /* clipboard write failed */
+      });
   }
