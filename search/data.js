0df4f691e40e02f4b7db62ed6ea5e900508cccb9
wanofuhito <116492669+wanofuhito@users.noreply.github.com>
2022-11-05 16:03:06 +0900

    Create data.js

---
+++ search/data.js
@@ -0,0 +1,5 @@
+var data = [
+  { url:"https://wanofuhito.github.io/nihon-shoki/viewer.html?kan=13rolls.txt", title:"十三巻", body:"日本書紀　巻第十三　雄朝津間稚子宿禰天皇 允恭天皇　雄朝津間稚子宿禰天皇、瑞齒別天皇同母弟也。天皇、自岐㠜至於總角、仁惠儉下、及壯篤病、容止不便。五年春正月、瑞齒別天皇崩。" },
+  { url:"https://wanofuhito.github.io/nihon-shoki/viewer.html?kan=14rolls.txt", title:"十四巻", body:"日本書紀　巻第十四　大泊瀬幼武天皇　雄略天皇　大泊瀬幼武天皇、雄朝嬬稚子宿禰天皇第五子也。天皇、産而神光滿[レ]殿、長而伉健過[レ]人。" },
+  { url:"https://wanofuhito.github.io/nihon-shoki/viewer.html?kan=14rolls.txt", title:"十四巻", body:"日本書紀　巻第十五　白髮武廣國押稚日本根子天皇 淸寧天皇　白髮武廣國押稚日本根子天皇、大泊瀬幼武天皇第三子也、母曰葛城韓媛。天皇、生而白髮、長而愛民、大泊瀬天皇於諸子中特所靈異。廿二年、立爲皇太子。" },
+];
