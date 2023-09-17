6bce9abc462cb15bf3395612d21e8c63dcbcad5d
wanofuhito <116492669+wanofuhito@users.noreply.github.com>
2023-09-17 18:03:00 +0900

    Update tategaki.js

--- yamatofumi/tategaki.js
+++ yamatofumi/tategaki.js
@@ -2,7 +2,7 @@
     const url = window.location.href;
     const pathname = location.pathname.split("/");
     console.log("pathname="+pathname);
-    const foldername = pathname[pathname.length - 2] + '/' + pathname[pathname.length - 1]  ;
+    const foldername = pathname[pathname.length - 3] + '/' + pathname[pathname.length - 2]  ;
     const download_url = window.location.protocol + '//' + window.location.hostname + '/' + foldername + '/' + filename;
     console.log("download_url="+download_url);
     return download_url;
